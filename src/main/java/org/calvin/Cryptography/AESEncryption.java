787dad192047ec443374cce3b6df024c6ac55f58
stalk-calvin <stalk.calvin@gmail.com>
2016-11-22 17:08:01 -0800

    CL: Add AES Encryption logic

---
+++ src/main/java/org/calvin/Cryptography/AESEncryption.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Calvin Lee - 2016.
+ * All Rights Reserved.
+ */
+
+package org.calvin.Cryptography;
+
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class AESEncryption {
+
+    private static final String ALGO = "AES";
+    private static final byte[] keyValue =
+            new byte[] { 'T', 'h', 'e', 'B', 'e', 's', 't',
+                    'S', 'e', 'c', 'r','e', 't', 'K', 'e', 'y' };
+
+    public static String encrypt(String Data) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(Data.getBytes());
+        String encryptedValue = new BASE64Encoder().encode(encVal);
+        return encryptedValue;
+    }
+
+    public static String decrypt(String encryptedData) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        String decryptedValue = new String(decValue);
+        return decryptedValue;
+    }
+
+    private static Key generateKey() throws Exception {
+        Key key = new SecretKeySpec(keyValue, ALGO);
+        return key;
+    }
+
+}
---
+++ src/test/java/org/calvin/Cryptography/AESEncryptionTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Calvin Lee - 2016.
+ * All Rights Reserved.
+ */
+
+package org.calvin.Cryptography;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.google.common.base.Strings;
+
+public class AESEncryptionTest {
+    private static final String password = "mypassword";
+    @Test
+    public void shouldEncrypAndDecrypt() throws Exception {
+        String passwordEnc = AESEncryption.encrypt(password);
+        assertFalse(Strings.isNullOrEmpty(passwordEnc));
+        assertFalse(passwordEnc.equals(password));
+
+        String passwordDec = AESEncryption.decrypt(passwordEnc);
+        assertTrue(passwordDec.equals(password));
+    }
+
+
+}

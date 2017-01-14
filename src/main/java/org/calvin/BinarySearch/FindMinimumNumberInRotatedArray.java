dbe6120b05bd37fa9c7557c82a1913ab3d0cefbb
stalk-calvin <stalk.calvin@gmail.com>
2017-01-14 15:25:05 -0800

    CL: Few Algorithms around rotated array

---
+++ src/main/java/org/calvin/BinarySearch/FindMinimumNumberInRotatedArray.java
@@ -0,0 +1,14 @@
+package org.calvin.BinarySearch;
+
+public class FindMinimumNumberInRotatedArray {
+    public int findMin(int[] nums) {
+        int start=0,mid=0,end=nums.length-1;
+        while(start<end){
+            mid=start+(end-start)/2;
+            if(nums[mid]>nums[end]) start=mid+1;
+            else if(nums[mid]==nums[end]) end--;
+            else end=mid;
+        }
+        return nums[start];
+    }
+}
---
+++ src/main/java/org/calvin/BinarySearch/FindNumberInRotatedArray.java
@@ -0,0 +1,25 @@
+package org.calvin.BinarySearch;
+
+public class FindNumberInRotatedArray {
+    public int find(int[] A, int key) {
+        int L = 0;
+        int R = A.length - 1;
+        while (L <= R) {
+            int M = L + ((R-L) / 2);
+            if (A[M] == key) {
+                return key;
+            } else if (A[M] >= A[L]) {
+                if (A[L] <= key && key < A[M])
+                    R = M - 1;
+                else
+                    L = M + 1;
+            } else {
+                if (A[M] < key && key <= A[R])
+                    L = M + 1;
+                else
+                    R = M - 1;
+            }
+        }
+        return -1;
+    }
+}

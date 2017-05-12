66437cb946c58dc1438ab207723cbb8dea5d3617
stalk-calvin <stalk.calvin@gmail.com>
2017-05-12 14:33:01 -0700

    CL: Fixes on readme etc

--- src/main/java/org/calvin/Numbers/CountPrimes.java
+++ src/main/java/org/calvin/Numbers/CountPrimes.java
@@ -11,16 +11,44 @@ import java.util.List;
 public class CountPrimes {
     public List<Integer> countPrimes(int n) {
         List<Integer> result = new ArrayList<>();
-        boolean[] m = new boolean[n];
-        for (int i = 2; i < n; i++) {
+        boolean[] m = new boolean[n+1];
+        for (int i = 2; i <= n; i++) {
             if (m[i])
                 continue;
 
             result.add(i);
-            for (int j = i; j < n; j = j + i)
+            for (int j = i; j <= n; j = j + i)
                 m[j] = true;
         }
 
         return result;
+    }
+
+    public int getNumberOfPrime(int n) {
+        // initially assume all integers are prime
+        boolean[] isPrime = new boolean[n+1];
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        // mark non-primes <= n using Sieve of Eratosthenes
+        for (int factor = 2; factor*factor <= n; factor++) {
+
+            // if factor is prime, then mark multiples of factor as nonprime
+            // suffices to consider mutiples factor, factor+1, ...,  n/factor
+            if (isPrime[factor]) {
+                for (int j = factor; factor*j <= n; j++) {
+                    isPrime[factor*j] = false;
+                }
+            }
+        }
+
+        // count primes
+        int primes = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) primes++;
+        }
+
+        return primes;
     }
 }
--- src/test/java/org/calvin/Numbers/CountPrimesTest.java
+++ src/test/java/org/calvin/Numbers/CountPrimesTest.java
@@ -23,10 +23,15 @@ public class CountPrimesTest {
 
     @Test
     public void shouldGetPrimeUptoGivenNumber() {
-        List<Integer> expected = Lists.newArrayList(2,3,5,7);
+        List<Integer> expected = Lists.newArrayList(2,3,5,7,11);
         List<Integer> actual = fixture.countPrimes(11);
         assertEquals(expected, actual);
-        assertEquals(4, actual.size());
+    }
+
+    @Test
+    public void shouldGetNumberOfPrimes() {
+        int actual = fixture.getNumberOfPrime(11);
+        assertEquals(5, actual);
     }
 
 }

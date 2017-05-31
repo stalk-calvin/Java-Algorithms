fc99aefe55d4df2e23eab7ffa9fb5bb64fe20de1
stalk-calvin <stalk.calvin@gmail.com>
2017-05-31 14:44:20 -0700

    CL: new algorithm for loading config

---
+++ src/test/java/org/calvin/LoadingProperties/Spring/ConfigTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Calvin Lee Since 2017.
+ * All Rights Reserved.
+ */
+
+package org.calvin.LoadingProperties.Spring;
+
+import static org.junit.Assert.*;
+
+import java.util.Properties;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConfigTest {
+    private static final String CONFIG_FILE = "config/spring.properties";
+    private Config fixture;
+
+    @Before public void setUp() throws Exception {
+        fixture = Config.builder().configFilename(CONFIG_FILE).build();
+    }
+
+    @Test public void shouldReadPropertiesFile() throws Exception {
+        Properties p = fixture.readPropertiesFile();
+        assertEquals("calvin", p.getProperty("database.username"));
+        assertEquals("jdbc:mysql://localhost:3306/calvin", p.getProperty("database.url"));
+        assertEquals("com.mysql.jdbc.Driver", p.getProperty("database.driver"));
+    }
+}

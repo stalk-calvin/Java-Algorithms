299dffbc27729f2f0ed640ddf387052fb77cb307
stalk-calvin <stalk.calvin@gmail.com>
2017-10-25 13:30:46 -0700

    more java algorithms, easy ones

--- src/main/java/org/calvin/BitManipulation/AddBinary.java
+++ src/main/java/org/calvin/BitManipulation/AddBinary.java
@@ -35,4 +35,23 @@ public class AddBinary {
         }
         return binaryDigit;
     }
+
+    public String addBinaryNatural(String a, String b) {
+        if (a == null) return b;
+        if (b == null) return a;
+        StringBuilder sb = new StringBuilder();
+        int aLength = a.length() - 1;
+        int bLength = b.length() - 1;
+        int carry = 0;
+        while (aLength + bLength >= 0) {
+            int sum = carry;
+            if (aLength >= 0) sum += a.charAt(aLength) - '0';
+            if (bLength >= 0) sum += b.charAt(bLength) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+            aLength--;
+            bLength--;
+        }
+        return sb.toString();
+    }
 }
--- src/main/java/org/calvin/Search/README.md
+++ src/main/java/org/calvin/Search/README.md
@@ -9,3 +9,14 @@ Plain version of binary search recursively.
 ## LinearSearch
 
 Shows how you can perform search linearly (sequential).
+
+## SearchInsertPosition
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it
+would be if it were inserted in order
+
+Ex:
+[1,3,5,6], 5 ? 2
+[1,3,5,6], 2 ? 1
+[1,3,5,6], 7 ? 4
+[1,3,5,6], 0 ? 0

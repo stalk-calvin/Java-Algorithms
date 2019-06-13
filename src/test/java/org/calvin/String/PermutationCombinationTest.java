6b3c7972818d3986c2cef1f9e7b34f59fb0d970a
stalk-calvin <stalk.calvin@gmail.com>
2019-06-13 08:07:11 -0700

    New Algorithm: Permute on every possibilities

--- src/test/java/org/calvin/String/PermutationCombinationTest.java
+++ src/test/java/org/calvin/String/PermutationCombinationTest.java
@@ -5,18 +5,24 @@
 
 package org.calvin.String;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class PermutationCombinationTest {
     private PermutationCombination fixture;
+
+    @Before
+    public void setUp() throws Exception {
+        fixture = new PermutationCombination();
+    }
 
     @Test
     public void shouldGeneratePermutation() throws Exception {
@@ -28,7 +34,6 @@ public class PermutationCombinationTest {
 
     @Test
     public void shouldGeneratePermutationArray() throws Exception {
-        fixture = new PermutationCombination();
         char[] input = {'a', 'b', 'c'};
         List<List<Character>> result = fixture.permute(input);
         Set<List<Character>> expected = Sets.newHashSet(
@@ -47,8 +52,17 @@ public class PermutationCombinationTest {
     }
 
     @Test
+    public void shouldGetAllPossibilities() throws Exception {
+        List<String> result = fixture.showPossibilities("AAB");
+        Set<String> expected = Sets.newHashSet("A", "AA", "AAB", "AB", "ABA", "B", "BA", "BAA");
+        for (String x : result) {
+            expected.remove(x);
+        }
+        assertTrue(expected.isEmpty());
+    }
+
+    @Test
     public void shouldGenerateAllPossibleCombination() throws Exception {
-        fixture = new PermutationCombination();
         char[] input = {'a', 'b'};
         fixture.combination(input, "", input.length, input.length);
         List<String> expected = Arrays.asList("aa", "ab", "ba", "bb");
@@ -57,7 +71,6 @@ public class PermutationCombinationTest {
 
     @Test
     public void shouldGenerateNoRepetitionCombination() throws Exception {
-        fixture = new PermutationCombination();
         fixture.combination3("", "ab");
         Set<String> expected = Sets.newHashSet("", "a", "ab", "b");
         assertEquals(expected, Sets.newHashSet(fixture.getResult()));

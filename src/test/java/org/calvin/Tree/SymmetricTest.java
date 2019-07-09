27aa0d8c3ad311f12160fc2d31a397b478a2de84
stalk-calvin <stalk.calvin@gmail.com>
2019-07-09 15:23:02 -0700

    New algorithm: mirror tree

--- src/test/java/org/calvin/Tree/SymmetricTest.java
+++ src/test/java/org/calvin/Tree/SymmetricTest.java
@@ -5,11 +5,12 @@
 
 package org.calvin.Tree;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.calvin.Utils.AssortedMethods;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
 
 public class SymmetricTest {
     Symmetric fixture;
@@ -26,4 +27,13 @@ public class SymmetricTest {
         assertTrue(fixture.isSymmetric(t1));
     }
 
+    @Test
+    public void shouldBeMirror() {
+        int[] input1 = {1,2,3,4,5,6,7,8};
+        TreeNode t1 = AssortedMethods.sortedArrayToBalancedBST(input1, 0, input1.length-1);
+        fixture.mirror(t1);
+        int[] inputExpected = {4,6,2,7,5,3,1,8};
+        TreeNode t2 = AssortedMethods.createTreeFromArray(inputExpected);
+        assertEquals(t2, t1);
+    }
 }
